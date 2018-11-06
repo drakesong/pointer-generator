43666f2bac6a3cff2686687f614285aff74da770
Drake Song <drakesong216@gmail.com>
2018-11-06 00:27:37 -0800

    Fix minor spelling error in comment.

--- run_summarization.py
+++ run_summarization.py
@@ -316,7 +316,7 @@ def main(unused_argv):
     decode_model_hps = hps._replace(max_dec_steps=1) # The model is configured with max_dec_steps=1 because we only ever run one step of the decoder at a time (to do beam search). Note that the batcher is initialized with max_dec_steps equal to e.g. 100 because the batches need to contain the full summaries
     model = SummarizationModel(decode_model_hps, vocab)
     decoder = BeamSearchDecoder(model, batcher, vocab)
-    decoder.decode() # decode indefinitely (unless single_pass=True, in which case deocde the dataset exactly once)
+    decoder.decode() # decode indefinitely (unless single_pass=True, in which case decode the dataset exactly once)
   else:
     raise ValueError("The 'mode' flag must be one of train/eval/decode")
 
