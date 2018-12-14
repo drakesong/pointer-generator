8d52706e0872b3f85d05b6c33c742b3221d000f3
Drake Song <drakesong216@gmail.com>
2018-12-13 19:30:12 -0800

    Add flag for using two_models.

--- decode.py
+++ decode.py
@@ -111,10 +111,12 @@ class BeamSearchDecoder(object):
       except ValueError:
         decoded_words = decoded_words
       decoded_output = ' '.join(decoded_words) # single string
-      decoded_output = add_pronouns(self._batcher._log_path, decoded_output)
+      if FLAGS.two_models:
+        decoded_output = add_pronouns(self._batcher._log_path, decoded_output)
 
       if FLAGS.single_pass:
-        decoded_words = re.findall(r"[\w']+|[^\s\w]", decoded_output)
+        if FLAGS.two_models:
+          decoded_words = re.findall(r"[\w']+|[^\s\w]", decoded_output)
         self.write_for_rouge(original_abstract_sents, decoded_words, counter) # write ref summary and decoded summary to file, to eval with pyrouge later
         counter += 1 # this is how many examples we've decoded
       else:
